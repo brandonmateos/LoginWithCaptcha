2c7434788d6bfccb55b27182f20749117f2d27bd
Wada8a <jochoa10@ucol.mx>
2024-03-13 20:56:15 -0600

    captcha jalando

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    let account = {
+        username: "wada",
+        password: "123456789ab"
+    };
+    let userInput = {
+        username: "",
+        password: ""
+    };
+    
+    function login() {
+        var captchaResponse = grecaptcha.getResponse();
+        userInput.username = $("#username").val();
+        userInput.password = $("#password").val();
+        if (captchaResponse.length != 0 && userValidation(userInput, account)) {
+            windowRedirection("./menu.html");
+        } else {
+            windowRedirection("./index.html");
+        }
+    }
+
+    function userValidation(userInput, account) {
+        if (
+            userInput.username == account.username &&
+            userInput.password == account.password
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function windowRedirection(page) {
+        window.location.assign(page);
+    }
+
+    $("#logoutButton").click(function() {
+        windowRedirection("./index.html");
+    });
+
+    $("#loginButton").click(login);
+});
